a30909b0f7460e52b4e86f1ab727bfaf03f01951
paigeman <53284808+paigeman@users.noreply.github.com>
2022-09-01 10:47:12 +0800

    update breakpoint.xml and optimize import

--- spring-boot-demo/config-registry/src/main/groovy/org/fade/demo/springframework/boot/registry/MyEnvironmentPostProcessor.groovy
+++ spring-boot-demo/config-registry/src/main/groovy/org/fade/demo/springframework/boot/registry/MyEnvironmentPostProcessor.groovy
@@ -1,6 +1,6 @@
 package org.fade.demo.springframework.boot.registry
 
-import org.apache.commons.logging.Log
+
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.boot.SpringApplication
